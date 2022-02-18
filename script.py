bb36fa1018769f912ce4074283c2d2f9171d692c
root <elizabethvinand@outlook.com>
2022-02-18 11:17:25 +0000

    please just work

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+
 name = input("Enter your name: ")
 print(f"Hello, {name}!")
 
@@ -5,3 +6,9 @@ if len(name) > 6:
     print("That's quite a long name!")
 else:
     print("That's quite a short name!")
+
+    print("Thanks for playing!")
+
+    print("Just kidding prosmise")
+    
+
